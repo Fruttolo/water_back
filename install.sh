04eb15eb52fd0a7a27b261aa6c92fc71c17cade9
Salvo <s.contino@gmail.com>
2024-09-17 19:23:09 +0200

    Fix curl installation command in install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Starting the backend"
 cd /home
 apt update
 apt install -y curl
-curl -sL https://deb.nodesource.com/setup_20.x | bash -
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
 apt install -y nodejs
 npm i
 node run migrate
