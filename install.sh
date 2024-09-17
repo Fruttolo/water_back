17e097f6a79df0640a6bfbb161b7e80a5ef7bad8
Salvo <s.contino@gmail.com>
2024-09-17 19:24:58 +0200

    Refactor install.sh to include curl installation and Node.js setup

--- install.sh
+++ install.sh
@@ -4,7 +4,12 @@ echo "Starting the backend"
 cd /home
 apt update
 apt install -y curl
-curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+apt install -y curl software-properties-common
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+source ~/.bashrc
+nvm install 20.11.0
+nvm use 20.11.0
+nvm alias default 20.11.0
 apt install -y nodejs
 npm i
 node run migrate
