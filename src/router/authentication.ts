2910c339ab1853745cdfd1c8e122a6d6593024d7
Salvo <s.contino@gmail.com>
2024-09-23 22:58:19 +0200

    Refactor authentication router to include authentication middleware in register route

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -3,7 +3,7 @@ import { register , login, refresh, check } from "../controllers/authentication"
 import { isAuthenticated } from '../middleware';
 
 export default (router: express.Router) => {
-    router.post('/auth/register', register);
+    router.post('/auth/register', isAuthenticated, register);
     router.post('/auth/login', login);
     router.post('/auth/refresh', refresh);
     router.get('/auth/check', isAuthenticated, check);
